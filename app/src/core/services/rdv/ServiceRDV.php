8aa477031b84e11fd3b89b3da90e7b6914ba08ac
Maxime Biechy <biechymaxime@gmail.com>
2024-09-10 17:02:36 +0200

    - Service RDV : ajout des répositories comme attributs

--- app/src/core/services/rdv/ServiceRDV.php
+++ app/src/core/services/rdv/ServiceRDV.php
@@ -5,9 +5,19 @@ namespace toubeelib\core\services\rdv;
 use toubeelib\core\domain\entities\praticien\Specialite;
 use toubeelib\core\dto\rdv\CreateRDVDTO;
 use toubeelib\core\dto\rdv\RDVDto;
+use toubeelib\infrastructure\repositories\ArrayPraticienRepository;
+use toubeelib\infrastructure\repositories\ArrayRdvRepository;
 
 class ServiceRDV implements ServiceRDVInterface
 {
+    private ArrayPraticienRepository $praticienRepository;
+    private ArrayRDVRepository $rdvRepository;
+
+    public function __construct()
+    {
+        $this->praticienRepository = new ArrayPraticienRepository();
+        $this->rdvRepository = new ArrayRdvRepository();
+    }
 
     public function creerRendezvous(CreateRDVDto $createRDVDTO): RDVDto
     {
@@ -16,7 +26,7 @@ class ServiceRDV implements ServiceRDVInterface
             throw new RDVPraticienNotFoundException();
         }
 
-        $specialitePraticien = new Specialite('','',''); // ! Récupère la spécialité du praticien;
+        $specialitePraticien = $this->praticienRepository->getPraticienById($createRDVDTO->praticien_id); // ! Récupère la spécialité du praticien;
         // ! Vérifie si la spécialité du praticien correspond à la spécialité du rendez-vous demandé
         if ($createRDVDTO->specialiteDM != $specialitePraticien) {
             throw new RDVSpecialitePraticienDifferentException();
