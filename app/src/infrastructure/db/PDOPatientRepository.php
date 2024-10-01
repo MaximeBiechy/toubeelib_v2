623929cd1f5f9bdf78e705f60f40a681247f8369
VadimElFamoso <vadimprofessionel@gmail.com>
2024-10-01 17:49:43 +0200

    PDOPatientRepository

--- app/src/infrastructure/db/PDOPatientRepository.php
+++ app/src/infrastructure/db/PDOPatientRepository.php
@@ -8,13 +8,31 @@ use toubeelib\core\repositoryInterfaces\PatientRepositoryInterface;
 class PDOPatientRepository implements PatientRepositoryInterface
 {
 
+    public function __construct(\PDO $pdo)
+    {
+        $this->pdo = $pdo;
+    }
+
     public function save(Patient $patient): string
     {
-        // TODO: Implement save() method.
+        $sql = "INSERT INTO patients (nom, prenom, adresse, tel, id) VALUES (:nom, :prenom, :adresse, :tel, :id)";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->bindValue(':nom', $patient->getNom());
+        $stmt->bindValue(':prenom', $patient->getPrenom());
+        $stmt->bindValue(':adresse', $patient->getAdresse());
+        $stmt->bindValue(':tel', $patient->getTel());
+        $stmt->bindValue(':id', $patient->getID());
+        $stmt->execute();
+        return $this->pdo->lastInsertId();
     }
 
     public function getPatientById(string $id): Patient
     {
-        // TODO: Implement getPatientById() method.
+        $sql = "SELECT * FROM patients WHERE id = :id";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->bindParam(':id', $id);
+        $stmt->execute();
+
+        return $stmt->fetch();
     }
 }
