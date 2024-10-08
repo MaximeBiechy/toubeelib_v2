6c22ef48934f3f28aae6da2940c0999db9b704ca
VadimElFamoso <vadimprofessionel@gmail.com>
2024-10-08 17:04:38 +0200

    signin action beta

---
+++ app/src/application/actions/SigninAction.php
@@ -0,0 +1,38 @@
+<?php
+namespace toubeelib\application\actions;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+
+use toubeelib\core\dto\auth\CredentialsDTO;
+use toubeelib\core\services\auth\AuthentificationServiceInterface;use toubeelib\core\services\auth\AuthentificationServiceNotFoundException;
+
+class SigninAction{
+  private AuthentificationServiceInterface $authentificationServiceInterface;
+
+  public function __construct(AuthentificationServiceInterface $authentificationServiceInterface)
+  {
+    $this->authentificationServiceInterface = $authentificationServiceInterface;
+  }
+
+  public function __invoke(ServerRequestInterface $rq, ResponseInterface $rs, array $args): ResponseInterface{
+
+    try {
+      $data = $rq->getParsedBody();
+      $email = $data["email"];
+      $password = $data["password"];
+
+      $credentialsDTO = new CredentialsDTO($email, $password);
+
+      $this->authentificationServiceInterface->byCredentials($credentialsDTO);
+
+      return $rs->withStatus(201);
+    }
+
+    catch (AuthentificationServiceNotFoundException $e) {
+
+    }
+  }
+}
+
+?>
