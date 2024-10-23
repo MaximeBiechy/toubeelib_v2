0d39907e2dfe95348c67ccc6e034e936dffd1e46
Timothée Benchergui <bencherg2u@etu.univ-lorraine.fr>
2024-10-23 12:57:55 +0200

    correction

--- app/src/application/provider/auth/JWTAuthProvider.php
+++ app/src/application/provider/auth/JWTAuthProvider.php
@@ -56,10 +56,10 @@ class JWTAuthProvider implements AuthProviderInterface
                 'aud' => 'toubeelib',
                 "iat" => time(),
                 "exp" => time() + 3600,
-                "sub" => $decoded->sub,
+                "sub" => $decoded['sub'],
                 "data" => [
-                    "email" => $decoded->data->email,
-                    "role" => $decoded->data->role
+                    "email" => $decoded['data']->email,
+                    "role" => $decoded['data']->role
                 ]
             ];
             $jwt = $this->jwtManager->createAccessToken($payload);
@@ -72,7 +72,7 @@ class JWTAuthProvider implements AuthProviderInterface
         }catch (\UnexpectedValueException $e) {
             throw new AuthProviderUnexpectedValueException('Token not valid');
         }
-        return new AuthDTO($decoded->sub, $decoded->data->email, $decoded->data->role, $jwt, $token);
+        return new AuthDTO($decoded['sub'], $decoded['data']->email, $decoded['data']->role, $jwt, $token);
     }
 
     public function getSignedInUser(string $token): AuthDTO
