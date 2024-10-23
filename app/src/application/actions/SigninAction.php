58b02cf19e25564f6ad4730c8f9ce269d0f7df90
Timothée Benchergui <bencherg2u@etu.univ-lorraine.fr>
2024-10-23 15:02:09 +0200

    signin via le header authorization

--- app/src/application/actions/SigninAction.php
+++ app/src/application/actions/SigninAction.php
@@ -6,6 +6,7 @@ use Psr\Http\Message\ServerRequestInterface;
 use Slim\Exception\HttpBadRequestException;
 use Slim\Exception\HttpInternalServerErrorException;
 use Slim\Exception\HttpNotFoundException;
+use Slim\Exception\HttpUnauthorizedException;
 use toubeelib\application\provider\auth\AuthProviderInterface;
 use toubeelib\application\renderer\JsonRenderer;
 use toubeelib\core\dto\auth\CredentialsDTO;
@@ -21,19 +22,22 @@ class SigninAction extends AbstractAction {
     }
 
     public function __invoke(ServerRequestInterface $rq, ResponseInterface $rs, array $args): ResponseInterface{
-        $data = $rq->getParsedBody();
-        if (!isset($data['email']) || !isset($data['password'])) {
-            throw new HttpBadRequestException($rq, 'missing email or password');
-        }
-        $email = $data['email'];
-        $password = $data['password'];
+        $token = $rq->getHeader('Authorization')[0] ?? throw new HttpUnauthorizedException($rq, 'missing Authorization Header');
+        $authHeader = sscanf($token, "Basic %s")[0] ;
+
+        $decodedAuth = base64_decode($authHeader);
+        list($email, $password) = explode(':', $decodedAuth, 2);
+
         try {
             $authDTO = $this->authnProviderInterface->signin(new CredentialsDTO($email, $password));
             $res = [
-                $authDTO->token,
-                $authDTO->token_refresh
+                'id' => $authDTO->id,
+                'email' => $authDTO->email,
+                'role' => $authDTO->role,
+                'token' => $authDTO->token,
+                'token_refresh' => $authDTO->token_refresh
             ];
-            return JsonRenderer::render($rs, 201, $res);
+            return JsonRenderer::render($rs, 200, $res);
         } catch (AuthentificationServiceNotFoundException $e) {
             throw new HttpNotFoundException($rq, $e->getMessage());
         } catch (AuthentificationServiceBadDataException $e) {
