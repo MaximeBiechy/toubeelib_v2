2a44823b48dac8eda12e1b20f94aa6276b9e7d96
MaximeBiechy <maxime.biechy5@etu.univ-lorraine.fr>
2025-01-08 16:40:18 +0100

    Création de la première action pour récupérer tous les praticiens

--- gateway/config/dependencies.php
+++ gateway/config/dependencies.php
@@ -1,6 +1,27 @@
 <?php
 
+use GuzzleHttp\Client;
+use GuzzleHttp\ClientInterface;
+use Psr\Container\ContainerInterface;
+use toubeelib\application\actions\ConsultingAllPraticiensAction;
+
 return [
 
+    ClientInterface::class => function(ContainerInterface $c) {
+        return new Client([
+            'base_uri' => 'http://localhost:6080/',
+            'headers' => [
+                'Accept' => 'application/json',
+                'Content-Type' => 'application/json',
+                'Origin' => 'http://localhost:6080'
+            ]
+        ]);
+    },
+
+
+    // ACTIONS
+    ConsultingAllPraticiensAction::class => function (ContainerInterface $c) {
+        return new ConsultingAllPraticiensAction($c->get(ClientInterface::class));
+    }
 
 ];
--- gateway/config/routes.php
+++ gateway/config/routes.php
@@ -1,7 +1,11 @@
 <?php
 declare(strict_types=1);
 
+use toubeelib\application\actions\ConsultingAllPraticiensAction;
+
 return function (\Slim\App $app): \Slim\App {
+
+    $app->get('/praticiens[/]', ConsultingAllPraticiensAction::class);
 
     return $app;
 };
