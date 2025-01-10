34edfe302d12e087925aeff273ae040313796fd6
MaximeBiechy <maxime.biechy5@etu.univ-lorraine.fr>
2025-01-10 14:35:34 +0100

    ajout de la route pour récupérer les infos d'1 praticien

--- gateway/config/routes.php
+++ gateway/config/routes.php
@@ -1,11 +1,12 @@
 <?php
 declare(strict_types=1);
 
-use toubeelib\application\actions\ConsultingAllPraticiensAction;
+use toubeelib\application\actions\PraticienAction;
 
 return function (\Slim\App $app): \Slim\App {
 
-    $app->get('/praticiens[/]', ConsultingAllPraticiensAction::class);
+    $app->get('/praticiens[/]', PraticienAction::class);
+    $app->get('/praticiens/{ID-PRATICIEN}[/]', PraticienAction::class);
 
     return $app;
 };
